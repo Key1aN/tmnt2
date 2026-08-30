5c17e98aad90979ddddb4dc1e83cd034bc9206cd
Key1aN <109429578+Key1aN@users.noreply.github.com>
2026-08-30 16:36:09 +0330

    Identify adaptive teleport v1 release trace

--- src/System/PC/main.cpp
+++ src/System/PC/main.cpp
@@ -24,7 +24,7 @@ _tWinMain(
 )
 {
     CPCCrashReporter::Install();
-    CPCCrashReporter::Breadcrumb("BUILD TMNT2_Intro_Skip_v1_US");
+    CPCCrashReporter::Breadcrumb("BUILD TMNT2_Slashuur_Adaptive_Teleport_v1_US");
 
 #ifdef VLDCHECK    
     VLDEnable();
